406d5409bee4f3d9e3dc3e3da8201e9faa7c5b5a
crashbussy <85747517+crashbussy@users.noreply.github.com>
2025-05-23 20:08:23 +0800

    Update tril_triu_grad_kernel_impl.h

--- paddle/phi/kernels/impl/tril_triu_grad_kernel_impl.h
+++ paddle/phi/kernels/impl/tril_triu_grad_kernel_impl.h
@@ -45,10 +45,6 @@ void TrilGradKernel(const Context& ctx,
                     const DenseTensor& out_grad,
                     int diagonal,
                     DenseTensor* x_grad) {
-  if (x_grad && x_grad->numel() == 0) {
-    ctx.template Alloc<T>(x_grad);
-    return;
-  }
 
   TrilTriuGradKernel<T, Context>(ctx, out_grad, diagonal, true, x_grad);
 }
@@ -58,10 +54,6 @@ void TriuGradKernel(const Context& ctx,
                     const DenseTensor& out_grad,
                     int diagonal,
                     DenseTensor* x_grad) {
-  if (x_grad && x_grad->numel() == 0) {
-    ctx.template Alloc<T>(x_grad);
-    return;
-  }
 
   TrilTriuGradKernel<T, Context>(ctx, out_grad, diagonal, false, x_grad);
 }
